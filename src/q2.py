c3b42c315e34a41cdc3bb45d7418d2f12cc9d24d
Harshit Varma <varmaharshit2@gmail.com>
2022-03-17 00:06:48 +0530

    added q2D: a-d

--- src/q2.py
+++ src/q2.py
@@ -1,4 +1,6 @@
 import re
+from operator import add
+from matplotlib import pyplot as plt
 from pyspark.sql import SparkSession
 
 # Constants
@@ -32,11 +34,40 @@ if __name__ == "__main__":
 
     pprint(g.take(5))
 
-    # Bad rows (takes time, uncomment before submitting)
-    # n_bad = g.filter(lambda row: row is None).count()
-    # print(f"Number of bad Rows : {n_bad}")
+    # Remove bad rows (takes time, uncomment before submitting)
+    g_clean = g.filter(lambda row: row is not None)
+    n_clean = g_clean.count()
+    n_total = g.count()
+    n_bad = n_total - n_clean
+    print(f"Number of bad Rows : {n_bad}")
 
+    # Status distribution
+    stat_distr = g_clean.map(lambda row: (row[3], 1)).reduceByKey(add).collect()
+    stat_distr = sorted(stat_distr, key = lambda t: int(t[0]))
+    print("HTTP status analysis:")
+    print("status\tcount")
+    for t in stat_distr:
+        print(f"{t[0]}\t{t[1]}")
 
+    # Pie chart
+    labels = [t[0] for t in stat_distr]
+    counts = [t[1] for t in stat_distr]
+    fig = plt.figure()
+    plt.pie(counts, labels = labels, autopct="%1.2f%%")
+    plt.savefig("q2Db.png")
+    plt.close(fig)
+
+    # Hosts distribution
+    host_distr = g_clean.map(lambda row: (row[0], 1)).reduceByKey(add).collect()
+    host_distr = sorted(host_distr, key = lambda t: t[0])
+    print("Frequent Hosts:")
+    print("host\tcount")
+    for t in host_distr:
+        print(f"{t[0]}\t{t[1]}")
+
+    # Unique hosts
+    print("Unique hosts:")
+    print(len(host_distr))
 
     # Stop
     spark.stop()
